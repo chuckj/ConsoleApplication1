659e479e5e6c1bc388f21dc385137d49493b63d3
chjohnson <chjohnson@nat.com>
2017-03-30 08:54:58 -0400

    add windows forms control and libraries

---
+++ WindowsFormsControlLibrary1/UserControl1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsControlLibrary1
+{
+    partial class EditableLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDisplay = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelDisplay
+            // 
+            this.labelDisplay.AutoSize = true;
+            this.labelDisplay.Location = new System.Drawing.Point(21, 16);
+            this.labelDisplay.Name = "labelDisplay";
+            this.labelDisplay.Size = new System.Drawing.Size(33, 13);
+            this.labelDisplay.TabIndex = 0;
+            this.labelDisplay.Text = "Label";
+            this.labelDisplay.DoubleClick += new System.EventHandler(this.labelDisplay_DoubleClick);
+            this.labelDisplay.Resize += new System.EventHandler(this.labelDisplay_Resize);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelDisplay);
+            this.groupBox1.Location = new System.Drawing.Point(6, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 46);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // EditableLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "EditableLabel";
+            this.Size = new System.Drawing.Size(152, 56);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDisplay;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class EditableLabel: UserControl
+    {
+        private TextBox editableTextBox;
+
+        public EditableLabel()
+        {
+            InitializeComponent();
+
+            editableTextBox = new TextBox();
+            this.groupBox1.Controls.Add(editableTextBox);
+            editableTextBox.KeyDown += new KeyEventHandler(editableTextBox_KeyDown);
+            editableTextBox.Hide();
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public override string Text
+        {
+            get { return labelDisplay.Text; }
+            set { labelDisplay.Text = value; }
+        }
+
+        private void labelDisplay_DoubleClick(object sender, EventArgs e)
+        {
+            editableTextBox.Size = labelDisplay.Size;
+            editableTextBox.Location = labelDisplay.Location;
+            editableTextBox.Text = labelDisplay.Text;
+            labelDisplay.Hide();
+            editableTextBox.Show();
+            editableTextBox.Focus();
+        }
+
+        void editableTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                labelDisplay.Text = editableTextBox.Text;
+                editableTextBox.Hide();
+                labelDisplay.Show();
+            }
+        }
+
+        private void labelDisplay_Resize(object sender, EventArgs e)
+        {
+            this.Size = labelDisplay.Size;
+        }
+    }
+}
